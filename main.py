d2b5c186ccd6d224a0af72d75dc39b04c2735847
Jeni Progcrammer <jennymarinay2@gmail.com>
2022-11-13 14:22:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def tic_tac_toe():
   pass
 
-def snake_game(): 
+def trivia(): 
   pass
 
 def rock_paper_scissors():    
@@ -13,12 +13,12 @@ def hangman_game():
 
 def main():
     while True:
-        print("1. Tic tac toe\n2. Snake Game\n3. Rock Paper Scissor\n4. Hangman Game\n5. Exit")
+        print("1. Tic tac toe\n2. Trivia\n3. Rock Paper Scissor\n4. Hangman Game\n5. Exit")
         choice = input("Enter your choice: ")
         if choice == "1":
             tic_tac_toe()
         elif choice == "2":
-            snake_game()
+            trivia()
         elif choice == "3":
             rock_paper_scissors()
         elif choice == "4":
@@ -27,3 +27,7 @@ def main():
             break
         else:
             print("Invalid choice")
+            
+    
+            
+            
